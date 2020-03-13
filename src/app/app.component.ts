1b67324f8b05f654947c191eb02f2fc57515c5ba
gambheera <lahiru.gambheera@gmail.com>
2020-03-13 13:46:31 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterViewInit {
+  name = 'Angular 6';
+  @ViewChild('editor') editor;
+
+  ngAfterViewInit() {
+
+    this.editor.getEditor().setOptions({
+      showLineNumbers: true,
+      tabSize: 2
+    });
+
+    this.editor.mode = 'javascript';
+    this.editor.value = `function testThis() {
+  console.log("it's working!")
+}`
+
+    this.editor.getEditor().commands.addCommand({
+      name: "showOtherCompletions",
+      bindKey: "Ctrl-.",
+      exec: function (editor) {
+
+      }
+    })
+  }
+
+  getValue() {
+    console.log(this.editor.value)
+    console.log(eval(this.editor.value));
+  }
+}
